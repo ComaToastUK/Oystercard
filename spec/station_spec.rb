0d76b438655e3f68f9fb6d79d9f015f3ec4c4799
ComaToastUK <jesusmanyouatethedog@gmail.com>
2017-09-04 16:58:57 +0100

    challange 13 complete, created Station class to store station zone info

---
+++ spec/station_spec.rb
@@ -0,0 +1,12 @@
+require 'station'
+
+RSpec.describe Station do
+  subject {described_class.new("Aldgate", 1)}
+  it 'knows station name' do
+    expect(subject.name).to eq ("Aldgate")
+  end
+
+  it 'knows station zone' do
+    expect(subject.zone).to eq 1
+  end
+end
