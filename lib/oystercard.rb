c6515a38f26f7f41529a834fe62dc9558889ad73
ComaToastUK <jesusmanyouatethedog@gmail.com>
2017-09-04 15:06:03 +0100

    add touch_in, touch_out, top_up, deduct

---
+++ lib/oystercard.rb
@@ -0,0 +1,45 @@
+class Oystercard
+
+  attr_reader :balance
+  DEFAULT_LIMIT = 90
+  DEFAULT_MINIMUM = 1
+  def initialize(limit = DEFAULT_LIMIT)
+    @balance = 0
+    @limit = limit
+    @in_use = false
+    @minimum = DEFAULT_MINIMUM
+  end
+
+  def top_up(num)
+    fail "Over limit £#{@limit}" if over_limit?(num)
+    @balance += num
+  end
+
+  def deduct(num)
+    @balance -= num
+  end
+
+  def over_limit?(num)
+    @balance + num >= @limit
+  end
+
+  def touch_in
+    fail 'Insufficient funds' if broke?
+    @in_use = true
+  end
+
+  def touch_out
+    @in_use = false
+  end
+
+  def in_journey?
+    @in_use
+  end
+
+  def broke?
+    @balance <= @minimum
+  end
+
+  private
+
+end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,5 +1,51 @@
 require 'oystercard'
 
-describe Oystercard do
+RSpec.describe Oystercard do
+  subject(:oystercard) { described_class.new }
+
+  describe '#initialize' do
+    it 'has a balance of 0' do
+      expect(subject.balance).to eq 0
+    end
+  end
+
+  describe '#top_up' do
+    it 'increases the balance by 1' do
+      expect{ subject.top_up 1 }.to change { subject.balance}.by 1
+      end
+    end
+
+    describe '#over_limit' do
+      it 'raises an error if over limit' do
+        message = "Over limit £#{Oystercard::DEFAULT_LIMIT}"
+        expect{subject.top_up(Oystercard::DEFAULT_LIMIT)}.to raise_error message
+      end
+    end
+
+    describe '#deduct' do
+      it 'deducts money from balance' do
+        subject.top_up(50)
+        expect{ subject.deduct(10) }.to change { subject.balance }.by -10
+      end
+    end
+
+    describe '#touch_in' do
+      it 'changes the oystercard status of in_use to true' do
+        subject.top_up(50)
+        expect { subject.touch_in }.to change { subject.in_journey? }.to true
+      end
+      it "raises_error if under £1" do
+        message = 'Insufficient funds'
+        expect{subject.touch_in}.to raise_error message
+      end
+    end
+
+    describe '#touch_out' do
+      it 'changes the oystercard status of in_use to false' do
+        subject.top_up(50)
+        subject.touch_in
+        expect { subject.touch_out }.to change { subject.in_journey? }.to false
+      end
+    end
 
 end
